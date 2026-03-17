ae5984d623d00b95af90a96d1783ef96f70a63d8
Vedant Choubey <vedantchoubey@Vedants-MacBook-Air.local>
2026-03-17 09:10:35 +0530

    uc2

--- app.java
+++ app.java
@@ -1,5 +1,10 @@
 public class HelloApp {
     public static void main (String[] args) {
-        System.out.println("Hello world!");
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, stranger! (Tip: Pass a name as an argument)");
+        }
     }
 }
