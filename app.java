a916a3fa12e4295bd44c6184d157f11eaf922d2a
Vedant Choubey <vedantchoubey@Vedants-MacBook-Air.local>
2026-03-17 09:36:15 +0530

    uc4

--- app.java
+++ app.java
@@ -1,9 +1,11 @@
-public class HelloApp {
+public class GreetingApp {
     public static void main(String[] args) {
-        String name = "World";
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
         }
-        System.out.println("Hello, " + name + "!");
     }
 }
